5edbc0c3e21d4041e6e30a9778cfc48b55892aa3
hydrastro <9256943+hydrastro@users.noreply.github.com>
2025-04-01 20:51:51 +0200

    removing unncessary file

--- primitives.c
+++ primitives.c
@@ -1,5 +1,4 @@
 #include "primitives.h"
-#include "deep_copy.h"
 #include "lizard.h"
 #include "mem.h"
 #include "parser.h"
@@ -931,4 +930,55 @@ lizard_ast_node_t *lizard_primitive_xnor(list_t *args, lizard_env_t *env,
                                          lizard_heap_t *heap) {
   lizard_ast_node_t *xor_result = lizard_primitive_xor(args, env, heap);
   return lizard_make_bool(heap, is_false(xor_result));
+}
+
+lizard_ast_node_t *lizard_ast_deep_copy(lizard_ast_node_t *node,
+                                        lizard_heap_t *heap) {
+  lizard_ast_node_t *copy;
+  if (!node) {
+    return NULL;
+  }
+  copy= lizard_heap_alloc(sizeof(lizard_ast_node_t));
+  copy->type = node->type;
+  switch (node->type) {
+  case AST_NUMBER:
+    mpz_init(copy->data.number);
+    mpz_set(copy->data.number, node->data.number);
+    break;
+  case AST_STRING:
+    copy->data.string = strdup(node->data.string);
+    break;
+  case AST_SYMBOL:
+    copy->data.variable = strdup(node->data.variable);
+    break;
+  case AST_BOOL:
+    copy->data.boolean = node->data.boolean;
+    break;
+  case AST_NIL:
+    return node;
+  case AST_QUOTE:
+  case AST_QUASIQUOTE:
+  case AST_UNQUOTE:
+  case AST_UNQUOTE_SPLICING:
+    copy->data.quoted = lizard_ast_deep_copy(node->data.quoted, heap);
+    break;
+  case AST_APPLICATION:
+    copy->data.application_arguments =
+        list_create_alloc(lizard_heap_alloc, lizard_heap_free);
+    {
+      list_node_t *iter;
+      for (iter = node->data.application_arguments->head;
+           iter != node->data.application_arguments->nil; iter = iter->next) {
+        lizard_ast_list_node_t *orig = (lizard_ast_list_node_t *)iter;
+        lizard_ast_list_node_t *copy_node =
+            lizard_heap_alloc(sizeof(lizard_ast_list_node_t));
+        copy_node->ast = lizard_ast_deep_copy(orig->ast, heap);
+        list_append(copy->data.application_arguments, &copy_node->node);
+      }
+    }
+    break;
+  default:
+    memcpy(&(copy->data), &(node->data), sizeof(node->data));
+  }
+  return copy;
 }
